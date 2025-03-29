4d432b818b89dc3a4b72820cff1218955cba26bf
Eugene Khristo <marteau9292@gmail.com>
2025-03-29 23:18:22 +0300

    Displaying Questions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,11 +4,13 @@ import Main from './Main';
 import Loader from './Loader';
 import Error from './Error';
 import StartScreen from './StartScreen';
+import Question from './Question';
 
 const initialState = {
   questions: [],
   //loading, error, ready, active, finished
   status: 'loading',
+  index: 0,
 };
 
 function reducer(state, action) {
@@ -17,6 +19,8 @@ function reducer(state, action) {
       return { ...state, questions: action.payload, status: 'ready' };
     case 'fetchFailed':
       return { ...state, status: 'error' };
+    case 'start':
+      return { ...state, status: 'active' };
 
     default:
       throw new Error('Action uknown');
@@ -24,7 +28,10 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
   const numQuestions = questions.length;
 
@@ -41,7 +48,10 @@ export default function App() {
       <Main>
         {status === 'loading' && <Loader />}
         {status === 'error' && <Error />}
-        {status === 'ready' && <StartScreen numQuestions={numQuestions} />}
+        {status === 'ready' && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === 'active' && <Question question={questions[index]} />}
       </Main>
     </div>
   );
